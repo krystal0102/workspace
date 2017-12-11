2443b6b066d44b075db5246e60c503c89ed6d5e7
krystal0102 <lsaj29@gmail.com>
2017-12-11 15:58:55 +0900

    Java Hello 프로그램 작성

---
+++ java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	// main : method, 프로그램의 시작점
+	// string[] args : arpument, parameter
+	public static void main (String[] args) {
+		// system.out.println 메서드 : 괄호 안의 글자를 출력
+		System.out.println("Hello, World");
+	}
+}
