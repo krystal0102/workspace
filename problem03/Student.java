c32d2f4cab96de85843195fdbda34ae439064ddf
krystal0102 <lsaj29@gmail.com>
2017-12-13 13:49:02 +0900

    Student 실습예제

--- problem03/Student.java
+++ problem03/Student.java
@@ -1,9 +1,11 @@
 class Student {
+	// 속성; 인스턴스 변수(instance Variable), 멤버 변수(Member variable)
 	int studentId;
 	String name;
 	int age;
 	String major;
 	int grade;
+	
 	
 	public Student(int studentId, String name, int age, 
 					String major, int grade){
@@ -13,16 +15,26 @@ class Student {
 						this.major = major;
 						this.grade = grade;
 					}
+	// 괄호 안에 들어가는 것들을 parameter, argument 라고 한다.
 	void study(){
 		System.out.println(this.name +"이(가)" + this.major+ "공부를 한다" );
 	}
-		
+	
 	void introduce () {
 		System.out.println (
-		"저는" + this.major+ this.grade + "학년 재학중인" +
-		+this.studentId + this.name + "입니다.");
+		"저는 " + this.major+ this.grade + "학년 재학중인 " +
+		+this.studentId +" "+ this.name + "입니다.");
 	}
-		
+	
+	//풀이, String을 사용할 경우!
+	// String study() {
+	// return this.name + "(이)가" + this.major + "공부를 한다.";
+	// }
+
+	//String introduce() {
+    // return "저는" + this.major + "" + this.grade + "학년" + "재학중인" + this.name + "입니다.";
+	//}
+
 	
 	
 }
--- problem03/TestDrive.java
+++ problem03/TestDrive.java
@@ -1,13 +1,13 @@
 public class TestDrive {
 		public static void main (String[] args) {
 			Student sujeong = 
-			new Student(171213, "수정", 25, "컴퓨터공학", 1);
+			new Student(171213, "이수정", 25, "컴퓨터공학", 1);
 			
 			Student sanghoon = 
-			new Student(171212, "상훈", 32, "기계공학", 2);
+			new Student(171212, "정상훈", 32, "기계공학", 3);
 			
 			Student sangil = 
-			new Student(171211, "상일", 35, "물리학", 4);
+			new Student(171211, "김상일", 35, "물리학", 4);
 			
 			sujeong.study ();
 			sujeong.introduce();
@@ -17,5 +17,14 @@ public class TestDrive {
 			
 			sangil.study ();
 			sangil.introduce();
+			
+			
+			//String을 사용한 경우
+			//
+			//String result1 = sujeong.study();
+			//System.out.println(result1);
+			//
+			//String result2 = sujeong.introduce();
+			//System.out.println(result2)
 		}
 }
