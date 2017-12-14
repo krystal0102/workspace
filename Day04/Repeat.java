3f85505b9b4881883159ee8abf13951d2f591262
krystal0102 <lsaj29@gmail.com>
2017-12-14 11:56:18 +0900

    반복문 for연습

---
+++ Day04/Repeat.java
@@ -0,0 +1,18 @@
+public class Repeat {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+		//수행하고자 하는 내용을 {}로 묶어줌
+		System.out.println("Hello World");
+		}
+		
+		
+		//args 배열 내용을 출력
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	
+	}
+	
+}
