9ca025055a856447a89c47b933d0770b50c0b096
krystal0102 <lsaj29@gmail.com>
2017-12-15 11:05:07 +0900

    실습문제07

--- problem07/Puppy.java
+++ problem07/Puppy.java
@@ -1,10 +1,12 @@
 class Puppy {
 	
+	// 인스턴스 변수
 	String type;
 	String name;
 	int age;
 	String gender;
 	
+	//Constructor
 	public Puppy(String type, String name, int age, String gender) {
 		
 		this.type = type;
@@ -13,15 +15,21 @@ class Puppy {
 		this.gender = gender;
 	}
 	
+	
+	//비교 메서드 만들기
 	boolean equals(Puppy p) {
+		
 		if (this.type == p.type) {
+			if (this.name == p.name) { 
+			//{}가 없으면 첫번째 줄만 if에 속한걸로 인식함
+				
+				return true;//강아지 종류와 이름이 같은 경우
+			}
 			
-			if (this.name == p.name)
-			return true;
+			return false; //강아지 종류는 같은데 이름이 다른 경우 "false"출력
 		}
 		
-		return false;
-		
+		return false; // 강아지 종류가 다르고 이름도 다른 경우
 			
 	}
 }
--- problem07/TestPuppy.java
+++ problem07/TestPuppy.java
@@ -5,9 +5,15 @@ public class TestPuppy {
 		Puppy duri = new Puppy("포메라니언","두리",2,"여");
 		Puppy ddoddo = new Puppy("말티즈","또또",6,"남");
 		
-		boolean isEqual01 = duri.equals(ddoddo);
-		System.out.println(isEqual01);
+		boolean isEqual = duri.equals(ddoddo);
 		
 		
+		if(isEqual){
+			System.out.println("같은 강아지다");
+		}
+		
+		else{
+			System.out.println("다른 강아지다");
+		}
 	}
 }
